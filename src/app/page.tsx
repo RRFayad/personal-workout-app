9d2546b1dbdf5c06820e3a965fa031749b1044e6
RRFayad <renan.r.fayad@gmail.com>
2024-08-21 08:27:58 -0300

    chore: Project Set Up

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Ihaa</h1>
+      <h2>Ihaa</h2>
+      <h3>Ihaa</h3>
+      <h4>Ihaa</h4>
+      <h5>Ihaa</h5>
+      <h6>Ihaa</h6>
+    </>
+  );
+}

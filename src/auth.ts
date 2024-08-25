a39a2fbe63600939f2ceaeea91feb5ae7b492420
RRFayad <renan.r.fayad@gmail.com>
2024-08-25 10:25:54 -0300

    fix(db schema): Fixing Prisma Schema do adjust to Next Auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,8 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/db";
-import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -17,36 +16,36 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "your-email@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const user = await db.user.findUnique({
-          where: { email: credentials?.email },
-        });
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "Email",
+    //       type: "email",
+    //       placeholder: "your-email@example.com",
+    //     },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize(credentials) {
+    //     const user = await db.user.findUnique({
+    //       where: { email: credentials?.email },
+    //     });
 
-        if (user && credentials?.password) {
-          // Compare the provided password with the hashed password in the database
-          const isValidPassword = await bcrypt.compare(
-            credentials.password,
-            user.password,
-          );
-          if (isValidPassword) {
-            return user; // Return the user object if authentication is successful
-          }
-        }
+    //     if (user && credentials?.password) {
+    //       // Compare the provided password with the hashed password in the database
+    //       const isValidPassword = await bcrypt.compare(
+    //         credentials.password,
+    //         user.password,
+    //       );
+    //       if (isValidPassword) {
+    //         return user; // Return the user object if authentication is successful
+    //       }
+    //     }
 
-        // If authentication fails, return null
-        return null;
-      },
-    }),
+    //     // If authentication fails, return null
+    //     return null;
+    //   },
+    // }),
   ],
   callbacks: {
     async session({ session, user }: any) {

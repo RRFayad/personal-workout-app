a219c767af47a2a63d3cda375b8d2ddb378b4c6d
RRFayad <renan.r.fayad@gmail.com>
2024-09-01 22:08:59 -0300

    chore(paths): Added paths file for better organization

---
+++ src/lib/paths.ts
@@ -0,0 +1,38 @@
+const paths = {
+  // Login OAuth - User start with OAuth, or Click to Login with email and PW or Click to Sign In
+  home() {
+    return "/";
+  },
+  credentialsLogin() {
+    return "/credentials";
+  },
+  credentialsSignUp() {
+    return "/credentials/signup";
+  },
+  createProfile() {
+    // Quiz Form
+    return "/create-profile";
+  },
+  // Profile & Meal Plan
+  profile() {
+    return "/profile";
+  },
+  // Workout Split
+  workoutSplit() {
+    return "/workout";
+  },
+  // Workout Plan
+  workoutPlan() {
+    return "/workout/plan";
+  },
+  // Workout Tracker
+  workoutTracker() {
+    return "/workout/tracker";
+  },
+  // Exercise Detail
+  exerciseDetail(exerciseId: string) {
+    return `/exercise/${exerciseId}`;
+  },
+};
+
+export default paths;

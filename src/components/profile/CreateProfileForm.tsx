14444aaf2c81dd9b15d3bafe1fc01852e1448f31
RRFayad <renan.r.fayad@gmail.com>
2024-09-11 09:42:05 -0300

    feat(WIP | CreateProfileForm):Start Creating the CreateProfileForm

---
+++ src/components/profile/CreateProfileForm.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+function CreateProfileForm() {
+  const form = useForm();
+
+  return (
+    <FormField
+      control={form.control}
+      name="username"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>This is your public display name.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+export default CreateProfileForm;

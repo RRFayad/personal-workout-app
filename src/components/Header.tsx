443c9034bfb6b59e078bdc47ee3494770aaf0029
RRFayad <renan.r.fayad@gmail.com>
2024-09-17 10:38:43 -0300

    style(avatar): Center loading spinner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,7 +53,7 @@ function Header() {
                     }
                   />
                   <AvatarFallback>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    <Loader2 className="h-4 w-4 animate-spin" />
                   </AvatarFallback>
                 </Avatar>
               </PopoverTrigger>

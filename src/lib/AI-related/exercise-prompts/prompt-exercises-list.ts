b739a77e5bb4f1b46b93bfd5e911b20bad0e292d
RRFayad <renan.r.fayad@gmail.com>
2024-09-27 09:48:11 -0300

    feat(createWorkout): WIP | Create the promptExercisesList

---
+++ src/lib/AI-related/exercise-prompts/prompt-exercises-list.ts
@@ -0,0 +1,21 @@
+import * as exercises from "@/lib/workout-exercises";
+
+type ExercisesType = typeof exercises;
+
+export const promptExercisesList: Record<string, any> = {};
+
+Object.keys(exercises).forEach((muscleGroup) => {
+  const muscleGroupExercises = exercises[muscleGroup as keyof ExercisesType];
+
+  promptExercisesList[muscleGroup] = {};
+
+  Object.keys(muscleGroupExercises).forEach((exerciseKey) => {
+    const exercise =
+      muscleGroupExercises[exerciseKey as keyof typeof muscleGroupExercises];
+
+    promptExercisesList[muscleGroup][exerciseKey] = {
+      intensity: exercise.intensity,
+      movementVariables: exercise.movementVariables,
+    };
+  });
+});
